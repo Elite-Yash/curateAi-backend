a08a568142e38b6ea5855b1edc864f857185ae65
AJ <ajaypatel.elitesigma@gmail.com>
2025-03-05 17:11:31 +0530

    Updated json message
    Updated json message

--- src/modules/comments/comments.controller.ts
+++ src/modules/comments/comments.controller.ts
@@ -1,29 +1,54 @@
-import { Controller, Post, Get, Param, Body, Delete } from "@nestjs/common";
-import { CommentsService } from "./comments.service";
-import { CreateCommentDto } from "./dto/create-comment.dto";
-import { Comment } from "./entities/comment.entity";
+import { Controller, Post, Get, Param, Body, Delete, HttpStatus } from '@nestjs/common';
+import { CommentsService } from './comments.service';
+import { CreateCommentDto } from './dto/create-comment.dto';
 
-@Controller("comments")
+@Controller('comments')
 export class CommentsController {
-  constructor(private readonly commentService: CommentsService) { }
+  constructor(private readonly commentsService: CommentsService) { }
 
   @Post()
-  async create(@Body() createCommentDto: CreateCommentDto): Promise<Comment> {
-    return this.commentService.create(createCommentDto);
+  async create(@Body() createCommentDto: CreateCommentDto) {
+    const comment = await this.commentsService.create(createCommentDto);
+    return {
+      statusCode: HttpStatus.CREATED,
+      message: 'Comment created successfully',
+      data: comment,
+    };
   }
 
   @Get()
-  async findAll(): Promise<Comment[]> {
-    return this.commentService.findAll();
+  async findAll() {
+    const comments = await this.commentsService.findAll();
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Comments retrieved successfully',
+      data: comments,
+    };
   }
 
-  @Get(":id")
-  async findOne(@Param("id") id: number): Promise<Comment> {
-    return this.commentService.findOne(id);
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    const comment = await this.commentsService.findOne(id);
+    if (!comment) {
+      return {
+        statusCode: HttpStatus.NOT_FOUND,
+        message: 'Comment not found',
+        data: null,
+      };
+    }
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Comment retrieved successfully',
+      data: comment,
+    };
   }
 
-  @Delete(":id")
-  async remove(@Param("id") id: number): Promise<void> {
-    return this.commentService.remove(id);
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    await this.commentsService.remove(id);
+    return {
+      statusCode: HttpStatus.OK,
+      message: 'Comment deleted successfully',
+    };
   }
 }
